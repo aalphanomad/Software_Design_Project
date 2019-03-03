1590f04883f5a1f514f0cd1d2dcee6caad43d140
H-IQ <48011944+H-IQ@users.noreply.github.com>
2019-03-03 21:35:51 +0200

    Update create.php

--- php SD/create.php
+++ php SD/create.php
@@ -4,16 +4,19 @@ $password = "s1601745";
 $database = "d1601745";
 $link = mysqli_connect("127.0.0.1", $username, $password, $database);
 $output=array();
+
 $name=$_REQUEST["name"];
 $studentnum=$_REQUEST["studentnum"];
 $email=$_REQUEST["email"];
 $password=$_REQUEST["password"];
 $transcript=$_REQUEST["transcript"];
+
 if (!isset($name, $studentnum, $email, $password, $transcript){
         $output["result"]="You didn't send the required values";
         echo json_encode($output);
         die();
 }
+    
 /* Select queries return a resultset */
 $result = mysqli_query($link, "INSERT INTO USER_INFORMATION(NAME, STUDENT_NUMBER, EMAIL_ADDRESS, PASSWORD, TRANSCRIPT) VALUES('$name','$studentnum','$email','$password','$transcript')");
 $output["result"]=$result;
