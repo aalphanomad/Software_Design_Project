699b19b452aea64e72995924e7760b91093055d1
H-IQ <48011944+H-IQ@users.noreply.github.com>
2019-03-03 21:37:38 +0200

    Update signin.php

--- php SD/signin.php
+++ php SD/signin.php
@@ -4,8 +4,10 @@ $password = "s1601745";
 $database = "d1601745";
 $link = mysqli_connect("127.0.0.1", $username, $password, $database);
 $output=array();
+
 $studentnum=$_REQUEST["studentnum"];
 $password=$_REQUEST["password"];
+
 /* Select queries return a resultset */
 if ($result = mysqli_query($link, "SELECT * from USER_INFORMATION where STUDENT_NUMBER='$studentnum'")) {
 while ($row=$result->fetch_assoc()){
