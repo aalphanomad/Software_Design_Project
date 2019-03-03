62bdaacabddcf990ed45c2827332416efdba8e75
H-IQ <48011944+H-IQ@users.noreply.github.com>
2019-03-03 21:33:05 +0200

    Update and rename signin.php.txt to signin.php

--- php SD/signin.php
+++ php SD/signin.php
@@ -4,10 +4,10 @@ $password = "s1601745";
 $database = "d1601745";
 $link = mysqli_connect("127.0.0.1", $username, $password, $database);
 $output=array();
-$username=$_REQUEST["username"];
+$studentnum=$_REQUEST["studentnum"];
 $password=$_REQUEST["password"];
 /* Select queries return a resultset */
-if ($result = mysqli_query($link, "SELECT * from USER where USER_NAME='$username'")) {
+if ($result = mysqli_query($link, "SELECT * from USER_INFORMATION where STUDENT_NUMBER='$studentnum'")) {
 while ($row=$result->fetch_assoc()){
 $output[]=$row;
 }
