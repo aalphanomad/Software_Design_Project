fd3ec46b60e1026bce1a1ffd835675eaa891a424
H-IQ <48011944+H-IQ@users.noreply.github.com>
2019-03-23 18:40:13 +0200

    Comments

--- HistoryClaims/CustomAdapter.java
+++ HistoryClaims/CustomAdapter.java
@@ -12,25 +12,25 @@ class CustomAdapter extends ArrayAdapter<String> {
 
 
     public CustomAdapter(Context context, String[] test) {
-        super(context, R.layout.customlist, test);
+        super(context, R.layout.customlist, test);  //receiving input such as course, date, etc.
     }
 
     public View getView(int position, View convertView, ViewGroup parent){
         LayoutInflater inflater = LayoutInflater.from(getContext());
-        View customView = inflater.inflate(R.layout.customlist, parent, false);
-
-        String date = getItem(0);
+        View customView = inflater.inflate(R.layout.customlist, parent, false); //setting our custom made view which will be returned
+        //getting specific deatils from array using getView method
+        String date = getItem(0);   
         String subject = getItem(1);
         String venue = getItem(2);
         String duration = getItem(3);
         System.out.println(getItem(0));
-
+        //declaring textviews
         TextView tvDate = (TextView) customView.findViewById(R.id.dateHistory);
         TextView tvSubject = (TextView) customView.findViewById(R.id.subjectHistory);
         TextView tvVenue = (TextView) customView.findViewById(R.id.venueHistory);
         TextView tvDuration = (TextView) customView.findViewById(R.id.durationHistory);
         TextView tvStatus = (TextView) customView.findViewById(R.id.status);
-
+        //setting textviews with appropriate information
         tvDate.setText("Date: " + date);
         tvSubject.setText("Subject: " + subject);
         tvVenue.setText("Venue: " + venue);
