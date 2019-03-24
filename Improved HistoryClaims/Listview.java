78c1031964d7a112fb20cc22f6e9188b24e619a5
H-IQ <48011944+H-IQ@users.noreply.github.com>
2019-03-24 12:52:49 +0200

    Add files via upload

---
+++ Improved HistoryClaims/Listview.java
@@ -0,0 +1,89 @@
+package com.example.projectgamma;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Listview extends AppCompatActivity {
+
+    ListView listview;
+    String subject = qrGenerator.Global.GetCourse();
+    String date = qrGenerator.Global.GetDate();
+    String venue = qrGenerator.Global.GetVenue();
+    String duration = "2hrs";
+
+    String[] mDate = {date};
+    String[] mSubject = {subject};
+    String[] mVenue = {venue};
+    String[] mDuration = {duration};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listview);
+
+        listview = findViewById(R.id.lv);
+
+        MyAdapter adapter = new MyAdapter(this, mDate, mSubject, mVenue, mDuration);
+        listview.setAdapter(adapter);
+
+        /*listview.setOnClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if(position==0){
+                    Toast.makeText(Listview.this, "Not Validated", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });*/
+    }
+
+    class MyAdapter extends ArrayAdapter<String>{
+        Context context;
+        String rDate[];
+        String rSubject[];
+        String rVenue[];
+        String rDuration[];
+
+        MyAdapter(Context c, String date[], String subject[], String venue[], String duration[]){
+            super(c, R.layout.customlist, R.id.dateHistory, date);
+            this.context = c;
+            this.rDate = date;
+            this.rSubject = subject;
+            this.rVenue = venue;
+            this.rDuration = duration;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = inflater.inflate(R.layout.customlist, parent, false);
+
+            TextView theDate = row.findViewById(R.id.dateHistory);
+            TextView theSubject = row.findViewById(R.id.subjectHistory);
+            TextView theVenue = row.findViewById(R.id.venueHistory);
+            TextView theDuration = row.findViewById(R.id.durationHistory);
+            TextView theStatus = row.findViewById(R.id.status);
+
+            theDate.setText(rDate[position]);
+            theSubject.setText("Subject: " + rSubject[position]);
+            theVenue.setText("Venue: " + rVenue[position]);
+            theDuration.setText("Duration: " + rDuration[position]);
+            theStatus.setText("Status: Not Validated");
+
+
+            return row;
+        }
+    }
+}
