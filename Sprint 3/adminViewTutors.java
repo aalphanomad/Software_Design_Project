57a5315df24caa6d160b12a409f6f543a2da9744
H-IQ <48011944+H-IQ@users.noreply.github.com>
2019-04-21 13:55:00 +0200

    Update adminViewTutors.java

--- Sprint 3/adminViewTutors.java
+++ Sprint 3/adminViewTutors.java
@@ -1,11 +1,35 @@
 package com.example.projectgamma;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Arrays;
 
 public class adminViewTutors extends AppCompatActivity {
 
@@ -14,8 +38,12 @@ public class adminViewTutors extends AppCompatActivity {
     ArrayAdapter<String> adapterOfTutors = null;
     String courseName, lecturerName;
     TextView courseTV, lecturerTV;
-    String[] tutors = {"Hamza", "Abdullah", "Mayur", "Innocent", "Elgoni"};
-    String[] lecturers = {"Mr 1", "Mr 2", "Mr 3", "Dr Opo", "Prof Marubini"};
+    String result;
+    String login_url = null;
+    String post_data = null;
+    String[] lecturers;
+    String[] tutors;
+    TextView Message, Message2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,20 +52,90 @@ public class adminViewTutors extends AppCompatActivity {
 
         //recieve value of string of the course that was clicked on in the previous activity
         Bundle bundle = getIntent().getExtras();
-        courseName=bundle.getString("courseView");
+        courseName = bundle.getString("courseView");
+
 
         //set the textview to show the clicked on cours
-        courseTV = (TextView)findViewById(R.id.tv_course2);
+        courseTV = (TextView) findViewById(R.id.tv_course2);
         courseTV.setText(courseName);
 
-        //the adapter initialised for our lecturer array to show all lecturers of the specific course in the listview
-        adapterOfLecturers = new ArrayAdapter(this, android.R.layout.simple_list_item_1, lecturers);
-        listview = findViewById(R.id.lv_lecturers);
-        listview.setAdapter(adapterOfLecturers);
+        Message=findViewById(R.id.Message);
 
-        //the adapter initialised for our tutors array to show all tutors of the specific course in the listview
-        adapterOfTutors = new ArrayAdapter(this, android.R.layout.simple_list_item_1, tutors);
-        listview = findViewById(R.id.lv_tutors);
-        listview.setAdapter(adapterOfTutors);
+        try {
+
+            //The code below recieves input from other classes to use the BackgroundWorker to send Booking/Claim form information to the server
+
+
+            //The URL to send data to the server when creating a booking/claim form
+            login_url = "http://lamp.ms.wits.ac.za/~s1601745/get_people.php";
+            System.out.println(courseName);
+            String[] dummy = courseName.split(" ");
+            post_data = URLEncoder.encode("course", "UTF-8") + "=" + URLEncoder.encode(dummy[0], "UTF-8");
+            URL url = new URL(login_url);
+            //The code below initializes an HTP POST request
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setDoInput(true);
+            OutputStream outputStream = httpURLConnection.getOutputStream();
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+            //Creating the URL in order to send data for generating a claim form to the server
+            bufferedWriter.write(post_data);
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            outputStream.close();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            result = "";
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                result += line;
+            }
+            bufferedReader.close();
+            inputStream.close();
+            httpURLConnection.disconnect();
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Log.i("tagconvertstr", "[" + result + "]");
+
+            JSONObject ja = new JSONObject(result);
+            lecturers = ja.getString("lecturers").split(",");
+            tutors = ja.getString("tutors").split(",");
+            qrGenerator.Global.formatter(tutors);
+            qrGenerator.Global.formatter(lecturers);
+            //the adapter initialised for our tutor array to show all lecturers of the specific course in the listview
+            if(!lecturers[0].equals("[]") && tutors!=null) {    //if array has lecturers then fill it onto the listview
+                adapterOfLecturers = new ArrayAdapter(this, android.R.layout.simple_list_item_1, lecturers);
+                listview = findViewById(R.id.lv_lecturers);
+                listview.setAdapter(adapterOfLecturers);
+
+                adapterOfTutors = new ArrayAdapter(this, android.R.layout.simple_list_item_1, tutors);
+                listview = findViewById(R.id.lv_tutors);
+                listview.setAdapter(adapterOfTutors);
+            }
+            else{//if there are no lecturers and tutors then send the message that it is empty
+
+                Message=findViewById(R.id.MessageForLecturer);
+                Message.setText("It's Empty Here...");
+
+                Message2=findViewById(R.id.MessageForTutors);
+                Message2.setText("It's Empty Here...");
+            }
+
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
