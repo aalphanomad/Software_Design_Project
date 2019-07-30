8b911fcae9a7ac655d5e3ca02415b2898559d499
H-IQ <48011944+H-IQ@users.noreply.github.com>
2019-07-30 11:06:58 +0200

    Add files via upload

--- claimForm/src/main/java/com/example/claimForm/MyUI.java
+++ claimForm/src/main/java/com/example/claimForm/MyUI.java
@@ -35,9 +35,11 @@ import com.vaadin.ui.VerticalLayout;
  */
 @Theme("mytheme")
 public class MyUI extends UI {
+	String course;
+    String venue;
+    String activity;
 
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
+    public MyUI () {
         final VerticalLayout verticallayout = new VerticalLayout();
         final HorizontalLayout horizontallayout = new HorizontalLayout();
         
@@ -45,31 +47,31 @@ public class MyUI extends UI {
         label.setId("label");
         label.setCaption("Claim Form");
         
-        ArrayList courses = new ArrayList();
-        courses.add("COMS3003 (Formal Languages and Automata)");
-        courses.add("COMS3005 (Advanced Analysis of Algorithms)");
-        courses.add("COMS3009 (Software Design)");
-        courses.add("COMS3010 (Operating Systems and System Programming)");
-        courses.add("COMS3007 (Machine Learning)");
-        courses.add("COMS3006 (Computer Graphics and Visualization)");
-        courses.add("COMS3008 (Parallel Computing)");
-        courses.add("COMS3011 (Software Design Project)");
+        ArrayList coursesArray = new ArrayList();
+        coursesArray.add("COMS3003 (Formal Languages and Automata)");
+        coursesArray.add("COMS3005 (Advanced Analysis of Algorithms)");
+        coursesArray.add("COMS3009 (Software Design)");
+        coursesArray.add("COMS3010 (Operating Systems and System Programming)");
+        coursesArray.add("COMS3007 (Machine Learning)");
+        coursesArray.add("COMS3006 (Computer Graphics and Visualization)");
+        coursesArray.add("COMS3008 (Parallel Computing)");
+        coursesArray.add("COMS3011 (Software Design Project)");
         
-        ArrayList activity = new ArrayList();
-        activity.add("Tutoring");
-        activity.add("Invigilating");
-        activity.add("Marking");
-        activity.add("Other");
+        ArrayList activityArray = new ArrayList();
+        activityArray.add("Tutoring");
+        activityArray.add("Invigilating");
+        activityArray.add("Marking");
+        activityArray.add("Other");
         
         ComboBox combobox = new ComboBox("Course");
         combobox.setWidth("100%");
-        combobox.setItems(courses);
+        combobox.setItems(coursesArray);
         
-        TextField venue = new TextField();
-        venue.setCaption("Venue");
+        TextField textfield = new TextField();
+        textfield.setCaption("Venue");
         
         ComboBox combobox2 = new ComboBox("Type of Activity");
-        combobox2.setItems(activity);
+        combobox2.setItems(activityArray);
         
         DateTimeField start = new DateTimeField();
         start.setCaption("Select start of duration");
@@ -79,19 +81,36 @@ public class MyUI extends UI {
         end.setCaption("Select end of duration");
         end.setValue(LocalDateTime.now());
         
-        Button confirm = new Button("Confirm");
-        //confirm.addClickListener(event -> Notification.show(combobox.getValue().toString(), Type.TRAY_NOTIFICATION));
         
         /*QRCode code = new QRCode();
         code.setValue("The quick brown fox jumps over the lazy dog");
         code.setWidth("400px");
         code.setHeight("400px");*/
         
-        verticallayout.addComponents(label, combobox, venue, combobox2, start, end, confirm);
         /*Button button = new Button("Register", e -> UI.getCurrent().navigate(Register.class));
          * add(button);
          */
         
+        if(!combobox.isEmpty() && !textfield.isEmpty() && !combobox2.isEmpty()) {
+	        course = combobox.getValue().toString();
+	        venue = textfield.getValue().toString();
+	        activity = combobox2.getValue().toString();
+        }
+        
+        else {
+        	combobox.setPlaceholder("Please fill in");
+        	textfield.setPlaceholder("Please fill in");
+        	combobox2.setPlaceholder("Please fill in");
+        }
+        String string = start.getValue().toString();
+        String timeStart = string.substring(11, string.length()-13);
+        String string2 = end.getValue().toString();
+        String timeEnd = string2.substring(11, string2.length()-13);
+        
+        Button confirm = new Button("Confirm");
+        confirm.addClickListener(event -> Notification.show(timeStart, Type.TRAY_NOTIFICATION));
+        
+        verticallayout.addComponents(label, combobox, textfield, combobox2, start, end, confirm);
         setContent(verticallayout);
     }
 
@@ -99,4 +118,10 @@ public class MyUI extends UI {
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
     }
+
+	@Override
+	protected void init(VaadinRequest request) {
+		// TODO Auto-generated method stub
+		
+	}
 }
