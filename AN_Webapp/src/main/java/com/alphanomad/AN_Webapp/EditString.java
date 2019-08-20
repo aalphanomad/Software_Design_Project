41393b84c46d681a55c4b282270046afa1f62839
H-IQ <48011944+H-IQ@users.noreply.github.com>
2019-08-20 11:42:20 +0200

    Update EditString.java

--- AN_Webapp/src/main/java/com/alphanomad/AN_Webapp/EditString.java
+++ AN_Webapp/src/main/java/com/alphanomad/AN_Webapp/EditString.java
@@ -3,13 +3,13 @@ package com.alphanomad.AN_Webapp;
 public class EditString {
 	
 	public static String editTime(String time) {
-        time= time.substring(11, time.length());
-        return time;
+		time= time.substring(11, time.length());
+		return time;
 	}
 	
 	public static String editCourse(String course) {
 		course= course.substring(0, 9);
-        return course;
+        	return course;
 	}
 
 }
