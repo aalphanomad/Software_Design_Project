6402de5e6aed41ccf8bb60336ef46efb49531d82
Elgoni <omer.elgoni@gmail.com>
2019-10-01 10:25:04 +0200

    Wrote a test for signin

---
+++ Tests/SignInTest.php
@@ -0,0 +1,51 @@
+<?php
+
+class HelloWorldTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var PDO
+     */
+    private $pdo;
+
+    public function setUp()
+    {
+        $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
+        //Create the USER_INFORMATION table
+        $this->pdo->query(" 	CREATE TABLE `USER_INFORMATION` (
+            `NAME` varchar(50) DEFAULT NULL,
+            `STUDENT_NUM` varchar(10) NOT NULL,
+            `EMAIL_ADDRESS` varchar(50) DEFAULT NULL,
+            `USER_PASSWORD` varchar(50) NOT NULL,
+            `ROLE` tinyint(1) DEFAULT NULL,
+            `COURSE_1` varchar(20) DEFAULT NULL,
+            `COURSE_2` varchar(20) DEFAULT NULL,
+            `COURSE_3` varchar(20) DEFAULT NULL,
+            `COURSE_4` varchar(20) DEFAULT NULL,
+            `COURSE_5` varchar(20) DEFAULT NULL,
+            `TRANSCRIPT` varchar(100) DEFAULT NULL,
+            UNIQUE KEY `STUDENT_NUMBER` (`STUDENT_NUM`)
+           ) ENGINE=InnoDB DEFAULT CHARSET=latin1");
+    }
+
+    public function tearDown()
+    {
+        $this->pdo->query("DROP TABLE hello");
+    }
+
+    public function testSignIn()
+    {
+        include '../fsignin.php';
+        
+        $this->assertEquals('{"result":1,"name":"Tutor","student_num":"1","role":"0"}', signin("1","test",$this->pdo));
+    }
+
+    public function testHello()
+    {
+        $helloWorld = new HelloWorld($this->pdo);
+
+        $this->assertEquals('Hello Bar', $helloWorld->hello('Bar'));
+    }
+}
+
