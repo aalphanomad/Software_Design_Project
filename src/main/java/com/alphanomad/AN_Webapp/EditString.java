6eb8b5bea5cbd0b48b51bdad63323d6470ab2e62
H-IQ <48011944+H-IQ@users.noreply.github.com>
2019-11-06 17:25:40 +0200

    Commenting

--- src/main/java/com/alphanomad/AN_Webapp/EditString.java
+++ src/main/java/com/alphanomad/AN_Webapp/EditString.java
@@ -2,7 +2,7 @@ package com.alphanomad.AN_Webapp;
 
 public class EditString
 {
-
+	//the sole purpose of this class is to simplify edit dirty/ugly strings for some classes outside
 	public static String editTime(String time)
 	{
 		time = time.substring(11, time.length());
