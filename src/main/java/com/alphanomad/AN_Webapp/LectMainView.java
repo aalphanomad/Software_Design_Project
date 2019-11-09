38c792c4109cae63d219a593c9b11535d2d348c2
H-IQ <48011944+H-IQ@users.noreply.github.com>
2019-11-09 15:30:35 +0200

    Fixed Lecturer view to show correct buttons

--- src/main/java/com/alphanomad/AN_Webapp/LectMainView.java
+++ src/main/java/com/alphanomad/AN_Webapp/LectMainView.java
@@ -151,6 +151,25 @@ public class LectMainView extends VerticalLayout implements View
 		grid.setHeaderRowHeight(30);
 		
 		
+		
+		//if the arraylist does have courses confirmed for the lecturer, then fill the combobox with these courses
+ 		if(myCourses!=null) {
+ 			combobox.setItems(myCourses);
+			addComponent(combobox);
+ 			combobox.setValue(myCourses.get(0));
+ 		}
+ 		
+ 		//else, give a warning to the user that they do not have access yet
+		else {
+ 			Label warn = new Label("<p style = \"font-family:georgia,garamond,serif;font-size:30px;\">\r\n"
+ 					+ "       <b><u>YOU DO NOT HAVE ACCESS</u></b> " + "      </p>", ContentMode.HTML);
+ 			addComponent(warn);
+ 			setComponentAlignment(warn, Alignment.MIDDLE_CENTER);
+ 			grid.setVisible(false);
+ 			
+ 		}
+		
+		
 
 		
 
@@ -260,27 +279,13 @@ public class LectMainView extends VerticalLayout implements View
 		addComponent(email_button);
 		addComponent(logout);
 		
-		
-		
-		
-		//if the arraylist does have courses confirmed for the lecturer, then fill the combobox with these courses
- 		if(myCourses!=null) {
- 			combobox.setItems(myCourses);
-			addComponent(combobox);
- 			combobox.setValue(myCourses.get(0));
- 		}
- 		
- 		//else, give a warning to the user that they do not have access yet
-		else {
- 			Label warn = new Label("<p style = \"font-family:georgia,garamond,serif;font-size:30px;\">\r\n"
- 					+ "       <b><u>YOU DO NOT HAVE ACCESS</u></b> " + "      </p>", ContentMode.HTML);
- 			addComponent(warn);
- 			setComponentAlignment(warn, Alignment.MIDDLE_CENTER);
- 			grid.setVisible(false);
+		//if the lecturer does not have any courses validated to lecture, remove email button
+		if(myCourses==null) {
  			email_button.setVisible(false);
  			setComponentAlignment(logout, Alignment.TOP_RIGHT);
- 			
  		}
+		
+	
 
 	}
 
