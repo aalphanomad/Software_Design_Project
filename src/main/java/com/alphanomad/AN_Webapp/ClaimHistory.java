2014854bbd4ba2e4b160b47b0304ba8fc59958af
Nevynnyy <1622535@students.wits.ac.za>
2019-11-11 00:17:46 +0200

    finished ClaimHistoryTest

--- src/main/java/com/alphanomad/AN_Webapp/ClaimHistory.java
+++ src/main/java/com/alphanomad/AN_Webapp/ClaimHistory.java
@@ -52,32 +52,10 @@ public class ClaimHistory extends VerticalLayout implements View
 	public void enter(ViewChangeEvent event)
 	{
 		removeAllComponents();
-
 		//Gets the username
 		UserInfo tutor_info = ((MyUI) getUI()).get_user_info();
 		String Tutor_Name = tutor_info.name;
-		String Tutor_StudentNum = tutor_info.student_num;
-
-		//Below,we are trying to get the number of bookings
-		DBHelper dbh = new DBHelper();
-		String[] params = { "name", "student_num" };
-		String[] values = { Tutor_Name, Tutor_StudentNum };
-		//Output is just a single integer specifying the number of bookings
-
-		int size = Integer.parseInt(dbh.php_request("no_of_bookings", params, values));
-		
-		//Gets all our past claims
-		//An Example output is {"dates":[{"DATE":"05 Apr 2019"}],"courses":[{"COURSE":"COMS2014"}],"start_time":[{"START_TIME":"08:51:00"}],
-		//"end_time":[{"END_TIME":"08:58:00"}],"valid":[{"VALID":"0"}],"venue":[{"VENUE":"hehe"}],"activity":[{"ACTIVITY":"Invigilating"}]}
-		
-		String ans = dbh.php_request("fetching", params, values);
-		if(!dbh.parse_json_string(ans).get("dates").toString().equals("[]")) {
-		String[][] Test = Display(ans, size);
-		Label test = new Label("<p style = \"font-family:georgia,garamond,serif;font-size:30px;\">\r\n"
-				+ "       <b><u>Your Claim History</u></b> " + "      </p>", ContentMode.HTML);
-		addComponent(test);
-		ArrayList<HistoryItem> History = new ArrayList<>();
-		
+		String Tutor_StudentNum = tutor_info.student_num;	
 */
 }
 
