a1ee140194a872e0b2bff6ce2c2eaa9181c08ae7
Nevynnyy <47993042+Nevynnyy@users.noreply.github.com>
2019-11-11 10:47:16 +0200

    Update TutorMainView.java

--- src/main/java/com/alphanomad/AN_Webapp/TutorMainView.java
+++ src/main/java/com/alphanomad/AN_Webapp/TutorMainView.java
@@ -49,23 +49,7 @@ public class TutorMainView extends VerticalLayout implements View
 		the_password=the_password.substring(1, the_password.length()-1);
 		System.out.println("byeeeeee  " + all.get(0) + " and " + all.get(1));
 		ArrayList<String> month_strs = new ArrayList<String>();
-		month_strs.add("Jan");
-		month_strs.add("Feb");
-		month_strs.add("Mar");
-		month_strs.add("Apr");
-		month_strs.add("May");
-		month_strs.add("Jun");
-		month_strs.add("Jul");
-		month_strs.add("Aug");
-		month_strs.add("Sep");
-		month_strs.add("Oct");
-		month_strs.add("Nov");
-		month_strs.add("Dec");
-		ComboBox<String> months = new ComboBox<String>();
-		months.setItems(month_strs);
-		months.setDescription("Choose month to generate claim form for. Leave empty to get full claim history");
-		//months.setCaption("Choose month to generate claim form for. Leave Blank to generate full record");
-		months.setEmptySelectionCaption("Choose month...");
+
 		
 	}*/
 
